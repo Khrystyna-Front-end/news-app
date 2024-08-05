d246dc5a7aea9152b44d926bbf05e6daee7d1d03
Khrystyna-Front-end <121770905+Khrystyna-Front-end@users.noreply.github.com>
2024-08-06 01:27:27 +0300

    FormSearch in App

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
       <div className="App">
         <Header />
         <CurrentDateTime date={new Date()} />
-        {/* <FormSearch newsData={news} /> */}
+        <FormSearch newsData={news} />
         <News dailyNews={news} />
 
         <Footer />
